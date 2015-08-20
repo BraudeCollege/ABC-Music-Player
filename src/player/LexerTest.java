e0d2912dff87c9e90f2b2b3ff5d1fca9cbba0035
ngnmhieu <ngnmhieu@gmail.com>
2015-08-20 14:36:32 +0200

    add 2 test case for 2 states

--- src/player/LexerTest.java
+++ src/player/LexerTest.java
@@ -2,8 +2,21 @@ package player;
 
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LexerTest
 {
+
+    @Test
+    public void testInitState()
+    {
+        assertTrue(new Lexer("ABC content").hasNext());
+    }
+
+    @Test
+    public void testEmptyString()
+    {
+        assertFalse(new Lexer("").hasNext());
+    }
 }
