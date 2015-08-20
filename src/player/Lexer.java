97a2489bffe8e5d8b2a598c626adc23af12e63b6
mducnguyen <m.duc.nguyen.93@gmail.com>
2015-08-20 14:38:13 +0200

    Add Operation : hasNext() and nextToken() to Lexer Class Create Token Class

--- src/player/Lexer.java
+++ src/player/Lexer.java
@@ -6,4 +6,29 @@ package player;
  */
 public class Lexer
 {
+    /**
+     * Text to be lexed
+     */
+    private String text;
+
+    public Lexer(String text) {
+        this.text = text;
+    }
+
+    /**
+     *
+     * @return true if there are tokens left; otherwise false
+     */
+    public boolean hasNext(){
+        return false;
+    }
+
+    /**
+     * This Method read the text of lexer and create some token
+     * @return nextToken if there're still some left
+     */
+    public Token nextToken(){
+        return null;
+    }
+
 }
