5357b968e27a6c3bf6e3e0007029bd86f98d346f
ngnmhieu <ngnmhieu@gmail.com>
2015-08-23 14:46:22 +0200

    add first datatype basenote; create parser and parser test

---
+++ src/player/ParserTest.java
@@ -0,0 +1,9 @@
+package player;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ParserTest
+{
+
+}
---
+++ src/player/ast/Basenote.java
@@ -0,0 +1,17 @@
+package player.ast;
+
+/**
+ * Represent a base note
+ */
+public class Basenote
+{
+    /**
+     * Symbol of basenote
+     */
+    public final char symbol;
+
+    public Basenote(char symbol)
+    {
+        this.symbol = symbol;
+    }
+}
