2dfd168b3f810b2268431a935fde55ef30946b14
ngnmhieu <ngnmhieu@gmail.com>
2015-08-23 16:59:54 +0200

    shorten Parser init in test, add expectOctave test

--- src/player/ParserTest.java
+++ src/player/ParserTest.java
@@ -1,5 +1,6 @@
 package player;
 
+import org.junit.Before;
 import org.junit.Test;
 import player.ast.Accidental;
 import player.ast.Basenote;
@@ -11,27 +12,22 @@ public class ParserTest
     @Test
     public void testBasenote()
     {
-        Lexer lex = new Lexer("A");
-        Parser pars = new Parser(lex);
-
-        assertEquals(new Basenote('A'),pars.expectBasenote());
+        Parser pars = getParser("A");
+        assertEquals(new Basenote('A'), pars.expectBasenote());
     }
 
 
     @Test(expected = Parser.UnexpectedTokenException.class)
     public void testBaseNoteFail() throws Exception
     {
-        Lexer lex = new Lexer("~");
-        Parser pars = new Parser(lex);
-
+        Parser pars = getParser("~");
         pars.expectBasenote();
     }
 
     @Test(expected = Lexer.RunOutOfTokenException.class)
     public void testBaseNoteEmptyLexer() throws Exception
     {
-        Lexer lex = new Lexer("");
-        Parser pars = new Parser(lex);
+        Parser pars = getParser("");
 
         pars.expectBasenote();
     }
@@ -40,21 +36,45 @@ public class ParserTest
     @Test
     public void testAccidental()
     {
-        Lexer lex = new Lexer("^^");
-        Parser pars = new Parser(lex);
-
+        Parser pars = getParser("^^");
         assertEquals(new Accidental(Accidental.Type.DOUBLE_SHARP), pars.expectAccidental());
+
+        pars = getParser("__");
+        assertEquals(new Accidental(Accidental.Type.DOUBLE_FLAT), pars.expectAccidental());
+
+        pars = getParser("=");
+        assertEquals(new Accidental(Accidental.Type.NEUTRAL), pars.expectAccidental());
     }
 
 
     @Test(expected = Parser.UnexpectedTokenException.class)
     public void testAccidentalFail() throws Exception
     {
-        Lexer lex = new Lexer("!");
-        Parser pars = new Parser(lex);
+        Parser pars = getParser("!");
 
         pars.expectAccidental();
     }
 
+    @Test
+    public void testOctave()
+    {
+        Parser parser  = getParser("'''");
+        assertEquals(new Octave(Octave.Type.UP, 3), parser.expectOctave());
 
+        parser  = getParser(",,");
+        assertEquals(new Octave(Octave.Type.DOWN, 2), parser.expectOctave());
+    }
+
+
+    @Test(expected = Parser.UnexpectedTokenException.class)
+    public void testOctaveFail() throws Exception
+    {
+        Parser pars  = getParser("xx");
+        pars.expectOctave();
+    }
+
+    public Parser getParser(String str)
+    {
+        return new Parser(new Lexer(str));
+    }
 }
