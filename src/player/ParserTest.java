d9822bcc60043d0dafe003f481a7876d8f5e04f0
mducnguyen <m.duc.nguyen.93@gmail.com>
2015-08-23 15:16:09 +0200

    Create testBasenote()

--- src/player/ParserTest.java
+++ src/player/ParserTest.java
@@ -1,9 +1,19 @@
 package player;
 
 import org.junit.Test;
+import player.ast.Basenote;
+
 import static org.junit.Assert.*;
 
 public class ParserTest
 {
+    @Test
+    public void testBasenote()
+    {
+        Lexer lex = new Lexer("A");
+        Parser pars = new Parser(lex);
+
+        assertEquals(new Basenote('A'),pars.expectBasenote());
+    }
 
 }
