6c0d299ea9ca19d78b18014a99fdd726eb824c19
ngnmhieu <ngnmhieu@gmail.com>
2015-08-23 23:41:06 +0200

    Done TODO NoteOrRest extends AbstractSyntaxTree

--- src/player/ast/NoteOrRest.java
+++ src/player/ast/NoteOrRest.java
@@ -3,6 +3,6 @@ package player.ast;
 /**
  * A NoteOrRest is either a Pitch or a Rest
  */
-public interface NoteOrRest extends AbstractSyntaxTree //TODO: Can be an NoteOrRest but not an AST
+public interface NoteOrRest extends AbstractSyntaxTree
 {
 }
