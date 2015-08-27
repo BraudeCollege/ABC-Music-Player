d58cc6588e53d00659f907bf9e41ae480028d456
mducnguyen <m.duc.nguyen.93@gmail.com>
2015-08-27 17:10:57 +0200

    Create AbcInfoCollector.java

---
+++ src/player/AbcInfoCollector.java
@@ -0,0 +1,123 @@
+package player;
+
+
+import player.ast.*;
+
+public class AbcInfoCollector implements AbcVisitor
+{
+
+
+    @Override
+    public void on(AbcTune tune)
+    {
+
+    }
+
+    @Override
+    public void on(AbcHeader header)
+    {
+
+    }
+
+    @Override
+    public void on(AbcMusic body)
+    {
+
+    }
+
+    @Override
+    public void on(FieldNumber field)
+    {
+
+    }
+
+    @Override
+    public void on(FieldTitle field)
+    {
+
+    }
+
+    @Override
+    public void on(FieldKey field)
+    {
+
+    }
+
+    @Override
+    public void on(FieldVoice field)
+    {
+
+    }
+
+    @Override
+    public void on(Comment c)
+    {
+
+    }
+
+    @Override
+    public void on(ElementLine line)
+    {
+
+    }
+
+    @Override
+    public void on(Element element)
+    {
+
+    }
+
+    @Override
+    public void on(NthRepeat repeat)
+    {
+
+    }
+
+    @Override
+    public void on(Barline bar)
+    {
+
+    }
+
+    @Override
+    public void on(TupletElement element)
+    {
+
+    }
+
+    @Override
+    public void on(Note note)
+    {
+
+    }
+
+    @Override
+    public void on(MultiNote note)
+    {
+
+    }
+
+    @Override
+    public void on(NoteLength noteLength)
+    {
+
+    }
+
+    @Override
+    public void on(Rest rest)
+    {
+
+    }
+
+    @Override
+    public void on(Pitch pitch)
+    {
+
+    }
+
+    @Override
+    public void on(Basenote basenote)
+    {
+
+    }
+}
---
+++ src/player/AbcInfoCollectorTest.java
@@ -0,0 +1,16 @@
+package player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbcInfoCollectorTest
+{
+    @Test
+    public void testGetDefaultLength()
+    {
+        AbcInfoCollector collector = new AbcInfoCollector();
+
+
+    }
+}
